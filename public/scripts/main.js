5ff9ecca1a66254001c6310e27605e6ab0845c1c
Triforcey <superlama01@gmail.com>
2017-12-30 23:40:27 -0700

    got some blocks to stack

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -16,10 +16,15 @@ var render = Render.create({
 // create two boxes and a ground
 var boxA = Bodies.rectangle(400, 200, 80, 80);
 var boxB = Bodies.rectangle(450, 50, 80, 80);
-var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+var boxC = Bodies.rectangle(500, -100, 80, 80);
+var boxD = Bodies.rectangle(450, -250, 80, 80);
+var ground = Bodies.rectangle(400, 610, 810, 60, {isStatic: true});
 
 // add all of the bodies to the world
-World.add(engine.world, [boxA, boxB, ground]);
+World.add(engine.world, [boxA, boxB, boxC, boxD, ground]);
+Matter.Body.applyForce(boxA, {x: boxA.position.x, y: boxA.position.y}, {x: .0128, y: 0});
+Matter.Body.applyForce(boxC, {x: boxC.position.x, y: boxC.position.y}, {x: -.01131, y: 0});
+//Matter.Body.applyForce(ground, {x: 0, y: ground.position.y}, {x: 0, y: -2});
 
 // run the engine
 Engine.run(engine);
